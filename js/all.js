1a943d0fef1ee618ddc9c04cdd6be6120c078d54
aandy300 <aandy300@gmail.com>
2021-08-18 15:42:00 +0800

    2021.08.18 jq改 增加點任何地方刪除class

--- js/all.js
+++ js/all.js
@@ -3,7 +3,19 @@ $(document).ready(function() {
    $('.showmenu').on('click',  function(e){
       // 停止<a>事件的默認動作 跳頁
       e.preventDefault();
+      e.stopPropagation();
       // 指定的 class 反向增加或刪除 指定 calss
       $('body').toggleClass('menu-show');
   });
+//   $(document).click(function(event){
+//    if (!$(event.target).hasClass('menu-show')) {
+//       //  $("#content").removeClass("mobile-open");
+//       e.preventDefault();
+//       $('body').removeClass('menu-show',false);
+//    }
+   // $('body').toggleClass('menu-show', false);
+   // });
+   $('body:not(.onclick-dropdown-link)').click(function(e){
+      $('body').removeClass('menu-show');
+    });
 });
