4926b1fd51dd2fd664793dd83e236e0dbdf1240c
aandy300 <aandy300@gmail.com>
2022-01-27 15:04:43 +0800

    2022.01.27 追加功能(草稿版) ok

--- Vue/homeWork/week3/js/list.js
+++ Vue/homeWork/week3/js/list.js
@@ -9,10 +9,34 @@ const app = Vue.createApp({
       products : {        
       },
       temp:{        
+      },
+      text: '123',
+      addProduct:{        
+        "data": {
+          "title": "[賣]動物園造型衣服3",
+          "category": "衣服2",
+          "origin_price": 100,
+          "price": 300,
+          "unit": "個",
+          "description": "Sit down please 名設計師設計",
+          "content": "這是內容",
+          "is_enabled": 1,
+          "imageUrl": "https://images.unsplash.com/photo-1516627145497-ae6968895b74?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1924&q=80",
+          "imagesUrl": [
+            "",
+            "",
+            "",
+            "",
+            ""
+          ]
+        }        
       }
     }
   },
-  methods: {   
+  methods: {  
+    testfunc(){
+      console.log(this.temp)
+    },
     checkLogin(){
       // token處理
       const token = document.cookie.replace(/(?:(?:^|.*;\s*)mingToken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
@@ -42,15 +66,27 @@ const app = Vue.createApp({
       .then((res) =>{        
         this.temp = {}
         this.getData()
+        console.log(delTargetId)
       })
       .catch((err) =>{
         console.dir(err)
         console.log(err)
       })
     },
-    openModal(){
-      console.log('first');
-      myModal.show()      
+    addProductPOST(){
+      axios.post( `${url}/api/${ path }/admin/product`, this.addProduct)
+      .then((res) =>{                
+        console.dir('新增商品成功')
+        this.getData()     
+      })
+      .catch((err) =>{
+        console.dir(err)
+        console.log(err)
+        alert(err.status + ' 請正確填寫商品資訊')
+      })  
+    },
+    openModal(){      
+      myModal.show()         
     },
     closeModal(){      
       myModal.hide()
@@ -60,6 +96,7 @@ const app = Vue.createApp({
     this.checkLogin()
     this.getData()
     myModal = new bootstrap.Modal(document.querySelector('#exampleModal'))
+    this.openModal()
   },
 
 
