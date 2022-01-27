d6ede7591796dea7c73f2e338a53f45eb9fae289
aandy300 <aandy300@gmail.com>
2022-01-27 10:31:19 +0800

    2022.01.27 w3hw b5 modal 測試完畢

---
+++ Vue/homeWork/week3/js/test.js
@@ -0,0 +1,69 @@
+// 課程 API 申請：https://vue3-course-api.hexschool.io/
+const url = 'https://vue3-course-api.hexschool.io/v2'
+const path = 'ming123'
+const modal = document.querySelector('#exampleModal')
+// var myModal = new bootstrap.Modal(modal) // 實體化
+let myModal = ''
+const app = Vue.createApp({
+  data(){
+    return{
+      products : {        
+      },
+      temp:{        
+      },
+      text:  '123'
+    }
+  },
+  methods: {   
+    checkLogin(){
+      // token處理
+      const token = document.cookie.replace(/(?:(?:^|.*;\s*)mingToken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+      axios.defaults.headers.common['Authorization'] = token
+      // token處理結束
+      axios.post( 'https://vue3-course-api.hexschool.io/v2/api/user/check' )
+      .then((res) =>{
+        console.log('checkLogin()', res)
+      })
+      .catch((err) =>{        
+        window.location = 'index.html'
+        alert('token錯誤，將轉回登入頁面!')
+      })
+    },
+    getData(){
+      axios.get( `${url}/api/${ path }/admin/products`)
+      .then((res) =>{                
+        this.products = res.data.products
+      })
+      .catch((err) =>{
+        console.dir(err)
+        console.log(err)
+      })
+    },
+    delData(delTargetId){
+      axios.delete( `${url}/api/${ path }/admin/product/${ delTargetId }`)
+      .then((res) =>{        
+        this.temp = {}
+        this.getData()
+      })
+      .catch((err) =>{
+        console.dir(err)
+        console.log(err)
+      })
+    },
+    openModal(){      
+      myModal.show()
+    },
+    closeModal(){      
+      myModal.hide()
+    },
+  },
+  mounted() {    
+    this.checkLogin()
+    this.getData()
+    // myModal = new bootstrap.Modal(modal) // modal沒用 要重新抓取
+    myModal = new bootstrap.Modal(document.querySelector('#exampleModal'))
+  },
+
+
+})
+app.mount('#app')
