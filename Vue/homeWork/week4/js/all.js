28eeec6f622d05012b9fef2668ac312cb84baede
aandy300 <aandy300@gmail.com>
2022-02-16 09:57:31 +0800

    import export 問題 暫時先用在 js解決 刪除、新增、編輯 modal OK 頁面 modal X

---
+++ Vue/homeWork/week4/js/all.js
@@ -0,0 +1,30 @@
+// 課程 API 申請：https://vue3-course-api.hexschool.io/
+const url = 'https://vue3-course-api.hexschool.io/v2'
+const path = 'ming123'
+const app = Vue.createApp({
+  data(){
+    return{
+      user : {
+        "username": "",
+        "password": ""
+      },
+    }
+  },
+  methods: {
+    siginIn(){
+      axios.post( `${url}/admin/signin`,this.user)
+        .then((res) => {
+          console.log(res)
+          const {token, expired} = res.data // 抓出 token & 到期時間
+          document.cookie = `mingToken=${ token }; expires=${ new Date(expired) }`          
+          window.location = 'list.html'
+        })
+        .catch((err) => {
+          console.dir(err)
+          console.log(err)          
+          alert(`${err.status}, ${err.data.message}`)
+        })
+    },    
+  },
+})
+app.mount('#app')
