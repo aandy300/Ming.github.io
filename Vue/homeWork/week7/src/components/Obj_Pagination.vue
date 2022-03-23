307e15d529c66b833fb6c18ce869c529b82ee588
aandy300 <aandy300@gmail.com>
2022-03-24 04:17:24 +0800

    fix 分頁按鈕 @click.prevent 無效 a 連結轉址

--- Vue/homeWork/week7/src/components/Obj_Pagination.vue
+++ Vue/homeWork/week7/src/components/Obj_Pagination.vue
@@ -2,7 +2,7 @@
     <nav aria-label="Page navigation example">
         <ul class="pagination">
         <li class="page-item" :class="{ disabled : !pages.has_pre }" >
-            <a class="page-link" href="#" aria-label="Previous" @click=" $emit('get-data', inside_pages.current_page - 1)" >
+            <a class="page-link" href="#" aria-label="Previous" @click.prevent=" $emit('get-data', inside_pages.current_page - 1)" >
             <span aria-hidden="true">&laquo;</span>
             </a>
         </li>
@@ -14,7 +14,7 @@
             <a class="page-link" href="" @click.prevent=" $emit('get-data', page) "> {{ page }} </a>
         </li>
         <li class="page-item" :class="{ disabled : !pages.has_next }" >
-            <a class="page-link" href="#" aria-label="Next" @click=" $emit('get-data', inside_pages.current_page + 1)" >
+            <a class="page-link" href="#" aria-label="Next" @click.prevent=" $emit('get-data', inside_pages.current_page + 1)" >
             <span aria-hidden="true">&raquo;</span>
             </a>
         </li>
