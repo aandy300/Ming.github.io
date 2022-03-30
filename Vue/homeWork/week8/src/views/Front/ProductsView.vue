a37ca0879bb2159eaf89c49d51288af063f5a4e5
aandy300 <aandy300@gmail.com>
2022-03-31 04:30:43 +0800

    v產品分類 @click 點擊後改 data === 會無效 一個 = 就好 fliter() 分流後 跑 商品array 篩選裡面物件的值 > 轉存篩選目標 > 回傳篩選目標

--- Vue/homeWork/week8/src/views/Front/ProductsView.vue
+++ Vue/homeWork/week8/src/views/Front/ProductsView.vue
@@ -2,17 +2,20 @@
     <!-- 分類 -->
     <nav class="navbar navbar-expand-lg navbar-light justify-content-center border border-start-0 border-end-0 border-top border-bottom">
       <div class="navbar-nav flex-row overflow-auto navbar-custom-scroll">
-        <a class="nav-item nav-link text-nowrap px-2" href="#">全部商品</a>
-        <a class="nav-item nav-link text-nowrap px-2" href="#">文物書籍</a>
-        <a class="nav-item nav-link text-nowrap px-2" href="#">周邊雜誌</a>
-        <a class="nav-item nav-link text-nowrap px-2" href="#">其他用品<span class="sr-only">(current)</span></a>
+        <!-- 點擊後改 data === 會無效 一個 = 就好 -->
+        <a class="nav-item nav-link text-nowrap px-2" @click="category = 'all'" onclick="event.preventDefault()" href="#">全部商品</a>
+        <a class="nav-item nav-link text-nowrap px-2" @click="category = 'type1'" onclick="event.preventDefault()" href="#">文物書籍</a>
+        <a class="nav-item nav-link text-nowrap px-2" @click="category = 'type2'" onclick="event.preventDefault()" href="#">周邊雜誌</a>
+        <a class="nav-item nav-link text-nowrap px-2" @click="category = 'type3'" onclick="event.preventDefault()" href="#">其他用品<span class="sr-only">(current)</span></a>
       </div>
     </nav>
     <div class="container mt-md-5 mt-3 mb-7">
       <div class="row">
         <!-- 商品單項 -->
         <!-- V-FOR -->
-        <div class="col-md-3" v-for="item in products" :key="item.id" >
+        <!-- <div class="col-md-3" v-for="item in products" :key="item.id" > -->
+        <!-- <div class="col-md-3" v-for="item in filterDate(products)" :key="item.id" > -->
+        <div class="col-md-3" v-for="item in filterDate(products)" :key="item.id" >
           <div class="card border-0 mb-4 position-relative">
             <!-- <img :src="item.imageUrl" class="card-img-top rounded-0" alt="..."> -->
             <div :style="{backgroundImage: `url(${item.imageUrl})`}"
@@ -37,7 +40,7 @@
               <!-- 原本的價目 -->
               <!-- <p class="text-muted mt-3">NT$ {{item.price}} / {{item.unit}}</p> -->
               <!-- <p class="text-muted mt-3">NT$ {{item.price}} / {{item.unit}}</p> -->
-              <button type="button" class="btn btn-secondary position-absolute bottom-0 start-0 mb-2">加入購物車</button>
+              <button type="button" class="btn btn-secondary position-absolute bottom-0 start-0 mb-2 disabled">加入購物車</button>
               <p class="mb-0 text-muted text-end mt-2"><del>NT$ {{item.origin_price}}</del></p>
               <p class="h4 fw-bold text-end mt-1 fs-5">NT$ {{item.price}}</p>
             </div>
@@ -69,6 +72,7 @@
 export default {
   data() {
     return {
+      category: 'all',
       products: []
     }
   },
@@ -84,6 +88,31 @@ export default {
       .catch((err) => {
         console.dir(err)
       })
+    },
+    // 分流後 跑 商品array 篩選裡面物件的值 > 轉存篩選目標 > 回傳篩選目標
+    filterDate(item){
+      console.log('filterDate')
+      if (this.category === 'all'){
+        return item
+      } else if (this.category === 'type1'){
+        // 篩選另存 > 回傳篩選結果
+        const newArray = item.filter(item =>
+          item.category === '分類一'
+        )
+        return newArray
+      } else if (this.category === 'type2'){
+        // 篩選另存 > 回傳篩選結果
+        const newArray = item.filter(item =>
+          item.category === '分類二'
+        )
+        return newArray
+      } else if (this.category === 'type3'){
+        // 篩選另存 > 回傳篩選結果
+        const newArray = item.filter(item =>
+          item.category === '分類三'
+        )
+        return newArray
+      }
     }
   },
   mounted() {
