bb5dbe96aea1e3ed923dfb9e392f050fca400e6b
aandy300 <aandy300@gmail.com>
2022-03-31 11:58:08 +0800

    換首頁樣板

--- Vue/homeWork/week8/src/components/FooterView.vue
+++ Vue/homeWork/week8/src/components/FooterView.vue
@@ -1,11 +1,25 @@
 <template>
-    <div class="bg-dark">
+    <!-- <div class="bg-dark">
       <div class="container">
         <div class="d-flex align-items-center justify-content-between text-white py-4">
           <p class="mb-0">© 2020 LOGO All Rights Reserved.</p>
           <ul class="d-flex list-unstyled mb-0 h4">
             <li><a href="#" class="text-white mx-3"><i class="fab fa-facebook"></i></a></li>
             <li><a href="#" class="text-white ms-3"><i class="fab fa-line"></i></a></li>
+          </ul>
+        </div>
+      </div>
+    </div> -->
+    <!--  -->
+
+    <div class="bg-dark py-1">
+      <div class="container">
+        <div class="d-flex align-items-center justify-content-between text-white mb-md-7 mb-4">
+          <a class="text-white h4" href="./index.html">LOGO</a>
+          <ul class="d-flex list-unstyled mb-0 h4">
+            <li><a href="#" class="text-white mx-3"><i class="bi bi-github"></i></a></li>
+            <li><a href="#" class="text-white mx-3"><i class="bi bi-github"></i></a></li>
+            <li><a href="#" class="text-white ms-3"><i class="bi bi-github"></i></a></li>
           </ul>
         </div>
       </div>
